2ace9f667a4880f467b1a16e498511b501525356
Max Cantor <max@maxcantor.net>
2015-07-21 14:32:49 -0400

    Stack 'em CSS.

--- lib/rack/scriptstacker.rb
+++ lib/rack/scriptstacker.rb
@@ -1,12 +1,5 @@
 require "rack/scriptstacker/version"
 
-class String
-  def smart_deindent
-    first_line_indent = self.match(/^\s*/).to_s.size
-    self.gsub(/^\s{#{first_line_indent}}/, '')
-  end
-end
-
 module Rack
   class ScriptStacker
     def initialize app
@@ -19,23 +12,32 @@ module Rack
       [
         response[0],
         response[1],
-        [replace_in_body(response[2])]
+        replace_in_body(response[2])
       ]
     end
 
     private
 
     def replace_in_body body
-      body[0].gsub /^(\s*)<<< JAVASCRIPT >>>/ do
-        indent = $1
-        js_files.map do |filename|
-          %Q[#{indent}<script type="text/javascript" src="/static/javascripts/#{filename}"></script>]
-        end.join("\n")
+      body.map do |chunk|
+        chunk.gsub /^(\s*)<<< JAVASCRIPT >>>/ do
+          indent = $1
+          js_files.map do |filename|
+            %Q[#{indent}<script type="text/javascript" src="/static/javascripts/#{filename}"></script>]
+          end.join("\n")
+        end.gsub /^(\s*)<<< CSS >>>/ do
+          indent = $1
+          css_files.map do |filename|
+            %Q[#{indent}<link rel="stylesheet" type="text/css" href="/static/css/#{filename}" />]
+          end.join("\n")
+        end
       end
-
     end
 
     def js_files
+    end
+
+    def css_files
     end
   end
 end
--- spec/scriptstacker_spec.rb
+++ spec/scriptstacker_spec.rb
@@ -1,29 +1,69 @@
 require 'rack/scriptstacker'
 
+def smart_deindent str
+  first_line_indent = str.match(/^\s*/).to_s.size
+  str.gsub(/^\s{#{first_line_indent}}/, '')
+end
+
 def app_with_body body
   lambda { |env| [nil, nil, [body]] }
 end
 
 describe Rack::ScriptStacker do
-  it 'injects javascript tags' do
-    middleware = Rack::ScriptStacker.new app_with_body(<<-HTML.smart_deindent)
-      <body>
-        <div>lmao</div>
-        <<< JAVASCRIPT >>>
-      </body>
-    HTML
+  let(:js_files) { ['main.js', 'util.js'] }
+  let(:css_files) { ['main.css', '_whatever.css'] }
+  let(:body) { '<div>whatever</div>' }
 
-    allow(middleware).to receive(:js_files).and_return(['main.js', 'util.js'])
-    response = middleware.call nil
-
-    expect(response[2][0]).to eq(<<-HTML.smart_deindent)
-      <body>
-        <div>lmao</div>
-        <script type="text/javascript" src="/static/javascripts/main.js"></script>
-        <script type="text/javascript" src="/static/javascripts/util.js"></script>
-      </body>
-    HTML
+  before :each do
+    @middleware = Rack::ScriptStacker.new app_with_body(body)
+    allow(@middleware).to receive(:js_files).and_return(js_files)
+    allow(@middleware).to receive(:css_files).and_return(css_files)
+    @response = @middleware.call nil
+    @response_body = @response[2][0]
   end
 
+  it 'does not change without replacement slots' do
+    expect(@response_body).to eq('<div>whatever</div>')
+  end
+
+  context 'javascript' do
+    let(:body) do
+      smart_deindent(<<-HTML)
+        <body>
+          <div>lmao</div>
+          <<< JAVASCRIPT >>>
+        </body>
+      HTML
+    end
+
+    it 'injects tags' do
+      expect(@response_body).to eq(smart_deindent(<<-HTML))
+        <body>
+          <div>lmao</div>
+          <script type="text/javascript" src="/static/javascripts/main.js"></script>
+          <script type="text/javascript" src="/static/javascripts/util.js"></script>
+        </body>
+      HTML
+    end
+  end
+
+  context 'css' do
+    let(:body) do
+      smart_deindent(<<-HTML)
+        <head>
+          <<< CSS >>>
+        </head>
+      HTML
+    end
+
+    it 'injects tags' do
+      expect(@response_body).to eq(smart_deindent(<<-HTML))
+        <head>
+          <link rel="stylesheet" type="text/css" href="/static/css/main.css" />
+          <link rel="stylesheet" type="text/css" href="/static/css/_whatever.css" />
+        </head>
+      HTML
+    end
+  end
 end
 
