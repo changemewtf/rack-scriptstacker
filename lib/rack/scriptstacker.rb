ecdb950398f7b355c5f0272fc28ae0e5b1441fc9
Max Cantor <max@maxcantor.net>
2015-07-21 13:07:36 -0400

    Better unit test for find/inject.

--- lib/rack/scriptstacker.rb
+++ lib/rack/scriptstacker.rb
@@ -1,9 +1,13 @@
 require "rack/scriptstacker/version"
 
+class ScriptFinder
+end
+
 module Rack
   class ScriptStacker
-    def initialize app
+    def initialize app, finder=nil
       @app = app
+      @finder = finder || ScriptFinder.new
     end
 
     def call env
@@ -12,16 +16,17 @@ module Rack
       [
         response[0],
         response[1],
-        replace_in_body(response[2])
+        [replace_in_body(response[2])]
       ]
     end
 
     private
 
     def replace_in_body body
-      body.map do |chunk|
-        chunk.gsub 'hey', 'sup yo'
-        chunk.gsub 'mate', 'wat'
+      body[0].gsub '<!-- ScriptStacker: JavaScript //-->' do
+        @finder.call.map do |filename|
+          '<script type="text/javascript" src="/static/javascripts/' + filename + '"></script>'
+        end.join("\n")
       end
     end
   end
--- spec/scriptstacker_spec.rb
+++ spec/scriptstacker_spec.rb
@@ -1,10 +1,30 @@
 require 'rack/scriptstacker'
 
-describe Rack::ScriptStacker do
-  it 'replaces in body' do
-    app = lambda { |env| [nil, nil, ['oi mate']] }
-    middleware = Rack::ScriptStacker.new app
-    response = middleware.call nil
-    expect(response[2]).to eq(['oi wat'])
-  end
+def app_with_body body
+  lambda { |env| [nil, nil, [body]] }
 end
+
+describe Rack::ScriptStacker do
+  it 'injects javascript tags' do
+    finder = lambda { ['main.js', 'util.js'] }
+
+    middleware = Rack::ScriptStacker.new app_with_body(<<-HTML), finder
+<body>
+<div>lmao</div>
+<!-- ScriptStacker: JavaScript //-->
+</body>
+    HTML
+
+    response = middleware.call nil
+
+    expect(response[2][0]).to eq(<<-HTML)
+<body>
+<div>lmao</div>
+<script type="text/javascript" src="/static/javascripts/main.js"></script>
+<script type="text/javascript" src="/static/javascripts/util.js"></script>
+</body>
+    HTML
+  end
+
+end
+
